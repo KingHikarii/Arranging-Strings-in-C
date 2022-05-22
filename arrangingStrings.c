b66684b9391e6eeac07b06ddd67e59a5a2e5c472
KingHikarii <103539937+KingHikarii@users.noreply.github.com>
2022-05-22 16:14:42 +0100

    Add files via upload
    Added the sorting logic, code can now sort the inputted strings, and display it.

--- arrangingStrings.c
+++ arrangingStrings.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void main()
 {
 
 
     int n;
+    char temp[30];
+
     printf("Enter a number which will be the max: ");
     scanf(" %d", &n);
 
@@ -15,13 +18,30 @@ void main()
 
     const char *strings[n][80];
 
-    for(int i = 0; i<=n; i++){
+    for(int i = 0; i<n; i++){
         scanf(" %s", strings[i]);
     }
-
-    for(int i = 0; i<=n; i++){
-        printf(" %s", strings[i]);
+    printf("Unsorted Array: ");
+    for(int i = 0; i<n; i++){
+        printf(" \n%s", strings[i]);
     }
+
+     for(int i=0; i<n; i++){
+    for(int j=0; j<n-1-i; j++){
+      if(strcmp(strings[j], strings[j+1]) > 0){
+        //swap array[j] and array[j+1]
+        strcpy(temp, strings[j]);
+        strcpy(strings[j], strings[j+1]);
+        strcpy(strings[j+1], temp);
+      }
+    }
+  }
+
+  //display the sorted array
+  printf("\n Sorted Array: ");
+   for(int i=0; i<n; i++){
+  printf(strings[i]);}
+
 
 
     }
